6f47251c3819fb7c5651fae19b1b226c6bb2390c
Jignesh <jigneshmal.com>
2024-03-08 22:29:07 +0530

    some corrections

--- src/app/_components/LoginComponent.jsx
+++ src/app/_components/LoginComponent.jsx
@@ -1,13 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Login.css"
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 const Login = () => {
+
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+
+    const handleLogin = () => {
+        if (!email || !password) {
+            toast.error("All Fields are Required !", {
+                position: toast.POSITION.TOP_CENTER,
+                className: "toast-message",
+            });
+            return false
+        }
+    }
+
     return (
         <div className='login-design'>
+            <ToastContainer/>
             <h1>Login</h1>
             <div className='login-input'>
-                <input type='text' placeholder='Email Id' className='input-text' />
-                <input type='password' placeholder='Password'  className='input-text'/>
-                <button >Login</button>
+                <input type='text' placeholder='Email Id' className='input-text' value={email} onChange={(e) => setEmail(e.target.value)} />
+                <input type='password' placeholder='Password' className='input-text' value={password} onChange={(e) => setPassword(e.target.value)} />
+                <button onClick={handleLogin}>Login</button>
             </div>
         </div>
     )
